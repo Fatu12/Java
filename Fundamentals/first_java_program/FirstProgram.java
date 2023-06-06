9e489b9cfd56004b277d8283036b9825002d0984
Fatuma Meshalla <mompround@gmail.com>
2023-06-06 00:37:46 -0700

    first commit

---
+++ Fundamentals/first_java_program/FirstProgram.java
@@ -0,0 +1,13 @@
+package Fundamentals.first_java_program;
+
+public class FirstProgram {
+
+
+    public static void main(String []arg){
+      System.out.println("My name is Coding Dojo");
+      System.out.println("I am 100 years old");
+     System.out.println("My hometown is Burbank, CA");
+    }
+ }
+    
+
