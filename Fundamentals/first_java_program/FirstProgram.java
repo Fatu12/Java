1c04b382a3f8b31923b704ac3d9773df1c68d2ce
Fatuma Meshalla <mompround@gmail.com>
2023-06-06 01:03:32 -0700

    llll

--- Fundamentals/first_java_program/FirstProgram.java
+++ Fundamentals/first_java_program/FirstProgram.java
@@ -1,12 +1,13 @@
 package Fundamentals.first_java_program;
 
 public class FirstProgram {
-
-
     public static void main(String []arg){
-      System.out.println("My name is Coding Dojo");
-      System.out.println("I am 100 years old");
-     System.out.println("My hometown is Burbank, CA");
+        String name = "Coding Dojo";
+        String homeTown = " Burbank, CA";
+        int age = 100;
+      System.out.println("My name is " + name );
+      System.out.println("I am "+ age +"years old ");
+     System.out.println("My hometown is " + homeTown);
     }
  }
     
