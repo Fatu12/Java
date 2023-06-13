72fad2d3696c3fbd5fb7974408b2be126d8e7d00
Fatuma Meshalla <mompround@gmail.com>
2023-06-12 23:41:05 -0700

    cafe business logic

--- Fundamentals/cafeBusinessLogic/CafeUtil.java
+++ Fundamentals/cafeBusinessLogic/CafeUtil.java
@@ -1,12 +1,11 @@
 import java.util.ArrayList;
-
-import javax.print.attribute.standard.Media;
+import java.util.Arrays;
 public class CafeUtil {
     // implement a reward system for customers who buy more drinks than 
     // they did the week before.
     // ? this method is going to sums together every consecutive integer from 1 to 10 and 
     // ? returns the sum
-    public Integer getStreakGoal(){
+    public int getStreakGoal(){
         int sum = 0;
        for (int i = 0; i< 11; i++){
           sum += i;
@@ -16,41 +15,62 @@ public class CafeUtil {
     // double getOderTotal giving list of prices from an order
     // ? this method is going to sum all the prices in the array 
     // ? and return the total
+    // this is fixed array type 
     public Double getOrderTotal(double[] prices){
-      double sum = 0;
-      for(double price: prices ){
-        sum += price;
+      double sumTotal = 0;
+      for(double price: prices){
+        sumTotal += price;
     }
-      return sum;
+      return sumTotal;
     }
-    // void displayMenu(ArrayList<String> menuItems)
-    // ? this method is going to print out each index and menu item from ArrayList
-  //  public void  displayMenu(ArrayList <String> menuItems){
+    // public  void displayMenu(ArrayList<String> menuItems)
+    //? this method is going to print out each index and menu item from ArrayList
+   public void  displayMenu(ArrayList<String> menuItems){
+    for(int i = 0; i < menuItems.size(); i++){
 
-  //   for(int i = 0; i < menuItems.size(); i++){
+        System.out.println(String.format( "%s %s",i, menuItems.get(i)));
+    // given an ArrayList of menu items(strings), print out each index and menu item
 
-  //       System.out.println( String.format( "this is the list of each coffee %s %s",i , menuItems.get(i)));
-  //   // given an ArrayList of menu items(strings), print out each index and menu item
-
-  //       // other operations using name
-  //   }
-    public void displayMenu(){
-      ArrayList<String> menuItems = new ArrayList<String>();
-      menuItems.add("drip coffee");
-      menuItems.add("cappuccino");
-      menuItems.add("latte");
-      System.out.println(menuItems);
-      for (int i = 0; i < menuItems.size(); i++){
-        System.out.println(i);
-        System.out.println(menuItems);
+        // other operations using name
+    }
+    // public void displayMenu(){
+    //   ArrayList<String> menuItems = new ArrayList<String>();
+    //   menuItems.add("drip coffee");
+    //   menuItems.add("cappuccino");
+    //   menuItems.add("latte");
+    //   System.out.println(menuItems);
+    //   for (int i = 0; i < menuItems.size(); i++){
+    //     System.out.println(i);
+    //     System.out.println(menuItems);
         
+    //   }
+
+
+
       }
+      public void addCustomer(ArrayList<String>customers){
+        boolean finished =false;
+        while(!finished){
+        System.out.println(" \n Please enter your name or press Q to quit: ");
+        String userName = System.console().readLine();
+        System.out.printf( "Hello %s",userName);
+        System.out.printf(" \n There  are %s  people in front of you ", customers.size());
+        // Checking if the user prase Q to quit 
+        if(userName.equals( "Q")){
+          finished = true;
+          return;
+        }
+        // this is going to add the user in the array and increment them up as we add your
+        customers.add(userName);
 
-
+          
+        }
       }
+          
+        }
+   
 
-
-    }
+      
 
 
     
--- Fundamentals/cafeBusinessLogic/CafeUtilTest.java
+++ Fundamentals/cafeBusinessLogic/CafeUtilTest.java
@@ -6,20 +6,31 @@ public class CafeUtilTest {
      public static void main(String[] args){
         CafeUtil appTest = new CafeUtil();
         int addNumber = appTest.getStreakGoal();
-        System.out.println(String.format("You need %s after 10 weeks to get reward " , addNumber));
-        double[] itemPrices = {4.5,3.5,2,4,5};
+        System.out.println("\n ======= Streak Goal Task =========");
+        System.out.println(String.format("Purchases needed by week 10: %s \n\n " , addNumber));
+        double[] itemPrices = {4.5,3.5,2.0,4.0,5.0};
          double totalPrice  = appTest.getOrderTotal(itemPrices);
+         System.out.println("===== Order Total Test =====");
         System.out.println(String.format("The total : %s \n",  totalPrice));
     // appTest.displayMenu( );
 
+            // ArrayList<String> coffeeMenu = new ArrayList<String>(Arrays.asList("Drip coffee", "cappuccino", "latte"));
+            // ArrayList<String> menu = new ArrayList<String>();
+            // menu.addAll(coffeeMenu);
 
 
         
-        List<String> coffeeMenu = Arrays.asList("drip coffee", "cappuccino","latte","mocha");
+        ArrayList<String> coffeeMenu =  new ArrayList<String>(Arrays.asList("drip coffee", "cappuccino","latte","mocha"));
         ArrayList<String> menu = new ArrayList<String>();
         menu.addAll(coffeeMenu);
-        System.out.println(coffeeMenu);
         appTest.displayMenu(menu);
+       
+        ArrayList<String> customers = new ArrayList<String>();
+        for(int i = 0; i < 1; i++) {
+            appTest.addCustomer(customers);
+            System.out.println("\n");
+        }
+        
 
         
        
